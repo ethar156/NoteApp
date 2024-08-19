b87f5a0df4aea2954359bdbdae8f37e42f5766e1
ethar <mahmoudethar501@gmail.com>
2024-08-19 16:09:02 +0300

    display notes part one

--- lib/cubit/notes_cubit/notes_state.dart
+++ lib/cubit/notes_cubit/notes_state.dart
@@ -5,16 +5,3 @@ abstract class NotesState {}
 
  class NotesInitial extends NotesState {}
 
- class NotesLoading extends NotesState {}
-
- class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSuccess(this.notes);
-}
-
-final class NotesFailure extends NotesState {
-  final String errmessage;
-
-  NotesFailure(this.errmessage);
-}
--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
 import 'package:note_app/views/widgets/notes_list_view.dart';
+import '../../cubit/notes_cubit/notes_cubit.dart';
 
-class NoteViewBody extends StatelessWidget {
-  const NoteViewBody({super.key});
+
+class NoteViewBody extends StatefulWidget {
+  const NoteViewBody({
+    super.key,
+  });
+
+@override
+State<NoteViewBody> createState() => _NoteViewBodyState();
+}
+
+class _NoteViewBodyState extends State<NoteViewBody> {
+  @override
+  void initState(){
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+      super.initState();
+  }
+  
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsetsDirectional.symmetric(horizontal: 16 ),
+      padding: const EdgeInsetsDirectional.symmetric(horizontal: 24 ),
       child: Column(
         children: const [
           SizedBox(
@@ -25,5 +42,5 @@ class NoteViewBody extends StatelessWidget {
       ),
     );
   }
-}
 
+}
